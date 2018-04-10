759b5cd0323ec2b41d4dd81988942250d2fad8da
hlolli <hlolli@gmail.com>
2018-04-11 01:43:43 +0200

    keep seperateing scsynth and scsynth-extras, due to 20mb artifact limit on clojars

---
+++ scsynth-extras/project.clj
@@ -0,0 +1,12 @@
+(defproject overtone/scsynth-extras "3.9.3-0"
+  :description "scsynth native libs with extras"
+  :url "http://overtone.github.io/"
+  :mailing-list {:name "overtone"
+                 :archive "https://groups.google.com/group/overtone"
+                 :post "overtone@googlegroups.com"}
+  :license {:name "The MIT License (MIT)"
+            :url "http://opensource.org/licenses/MIT"
+            :distribution :repo
+            :comments "Please use Overtone for good"}
+  
+  :resource-paths ["resources"])
